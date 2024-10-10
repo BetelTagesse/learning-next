36bec64cfda259325b2b463dbfdc6b5a7723b06e
BetelTagesse <BetelTagesse367@gmail.com>
2024-10-10 15:00:00 +0300

    Added dynamic routing

---
+++ app/layout.jsx
@@ -0,0 +1,21 @@
+import "@styles/globals.css";
+
+export const metadata = {
+  title: "promptopia",
+  description: "Discover & share AI Prompts",
+};
+
+const RootLayout = () => {
+  return (
+    <html lang="en">
+      <body>
+        <div className="main">
+          <div className="gradient" />
+        </div>
+        <main className="app">{children}</main>
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
