4c512e7349cf6f1544bf019d7ffafd8bdf81996b
BetelTagesse <BetelTagesse367@gmail.com>
2024-10-11 12:00:00 +0300

    added paragraph

--- app/layout.jsx
+++ app/layout.jsx
@@ -5,7 +5,7 @@ export const metadata = {
   description: "Discover & share AI Prompts",
 };
 
-const RootLayout = () => {
+const RootLayout = ({ children }) => {
   return (
     <html lang="en">
       <body>
