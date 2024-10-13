7bba040bba97754f1e5a3cd404f03cd02dd1b5af
BetelTagesse <BetelTagesse367@gmail.com>
2024-10-13 12:00:00 +0300

    added paragraph

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -23,13 +23,25 @@ const Nav = () => {
           <p className="logo_text">Promptopia</p>
         </Link>
 
-        {/* Mobile devices  */}
+        {/* Desktop Navigation  */}
 
         <div className="sm:flex hidden">
           {isUserLoggedIn ? (
             <div className="flex gap-3 md:gap-5">
               <Link href="/create-prompt" className="black_btn">
                 Create Post
+              </Link>
+              <button type="button" onClick={signOut} className="outline_btn">
+                Sign Out
+              </button>
+              <Link href="/profile">
+                <Image
+                  src="/assets/images/logo.svg"
+                  width={37}
+                  height={37}
+                  className="rounded-full"
+                  alt="profile"
+                />
               </Link>
             </div>
           ) : (
